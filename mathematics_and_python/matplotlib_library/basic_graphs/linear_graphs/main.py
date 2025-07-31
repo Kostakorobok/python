112a03c48cccfaa772db3bf6574716886c6e0b3e
Constantine <193867275+Kostakorobok@users.noreply.github.com>
2025-07-31 22:44:52 +1000

    31-07-2025: - Revising matplotlib library - adding some structure into learning python for mathematics

--- mathematics_and_python/matplotlib_library/basic_graphs/linear_graphs/main.py
+++ mathematics_and_python/matplotlib_library/basic_graphs/linear_graphs/main.py
@@ -7,19 +7,25 @@ y1 = []
 y2 = []
 y3 = []
 
+# Functions for y-axis to create 3 separate graphs
 for i in x:
     y1.append(i)
     y2.append(i*6 + i*3 + 7)
     y3.append(i*4)
 
+# plt.plot plots graph on the same space
+
 plt.plot(x, y1, color = "green")
 plt.plot(x, y2, color = "red")
 plt.plot(x, y3, color = "blue")
 
+# Checking generated values
 print(x,"\n", y1, "\n", y2, "\n", y3)
 
+# Annotating
 plt.suptitle("Linear Graphs")
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
 
+# Displays Graph on the screen
 plt.show()
