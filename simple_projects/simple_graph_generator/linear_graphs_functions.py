fcfa0b012ac66c1880ffe227df80fe317b408d5a
const <constantine.mathew.1991@gmail.com>
2025-09-07 18:23:39 +1000

    Python lesson 07-09-2025

--- simple_projects/simple_graph_generator/linear_graphs_functions.py
+++ simple_projects/simple_graph_generator/linear_graphs_functions.py
@@ -1,22 +1,23 @@
 import random
 import matplotlib.pyplot as plt
 
-def random_xy_values(x, y, m, c, no):
-    m = 1
-    c = 1
+def random_xy_values(x, y, no):
     for i in range(no):
         x_temp = random.randint(0, 100)
-        y_temp = x_temp * m + c
+        y_temp = x_temp + 1
         x.append(x_temp)
         y.append(y_temp)
 
-
-
 def generate_graph(x, y):
-    plt.plot(x, y, color="green", linestyle="solid", marker="o", linewidth=1)
+    plt.clf()
 
+    plt.plot(x, y, color="green", linestyle="solid", marker="o", linewidth=1)
     plt.suptitle("Random Linear Graph")
     plt.xlabel("x-axis values")
     plt.ylabel("y-axis values")
 
     plt.show()
+
+def clear_data(x, y):
+    x.clear()
+    y.clear()
--- simple_projects/simple_graph_generator/random_graph_generator.py
+++ simple_projects/simple_graph_generator/random_graph_generator.py
@@ -1,19 +1,17 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
 import linear_graphs_functions as lgf
 
-root = Tk()
+root = tk.Tk()
 root.title("Random Graph Generator")
-mainframe = ttk.Frame(root, padding = 10)
-mainframe.grid()
-
+root.geometry("600x400")
 x_axis = []
 y_axis = []
 
-ttk.Button(mainframe, text = "Generate x and y values", command = lgf.random_xy_values(x_axis, y_axis, 1, 1, 10)).grid(row = 0, column = 0)
-ttk.Button(mainframe, text = "Show Graph", command = lgf.generate_graph(x_axis, y_axis)).grid(row = 1, column = 0)
-ttk.Button(mainframe, text = "Quit", command = root.destroy).grid(row = 2, column = 0)
-ttk.Button(mainframe, text = "Clear Data").grid(row = 3, column = 0)
+buttonGenerate = tk.Button(root, width = 25, height = 2, text = "Generate x and y values", command =lambda: lgf.random_xy_values(x_axis, y_axis, 10)).pack(side="top", padx = 20, pady = 20)
+buttonClear = tk.Button(root,width = 25, height = 2, text = "Clear x and y data", command = lambda: lgf.clear_data(x_axis, y_axis)).pack(side="left", padx = 20, pady = 20)
+buttonShow = tk.Button(root,width = 25, height = 2, text = "Show Graph", command =lambda: lgf.generate_graph(x_axis, y_axis)).pack(side="right", padx = 20, pady = 20)
+buttonQuit = tk.Button(root,width = 25, height = 2, text = "Quit", command = root.destroy).pack(side="bottom", padx = 20, pady = 20)
 
 root.mainloop()
